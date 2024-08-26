2563b0540a6a1303e0a839bf43e0a5ef702f1208
GustavodiasDeOliveira <gustavodiasdeoliveira72@gmail.com>
2024-08-25 23:20:30 -0300

    primeiro commit

---
+++ assets/js/login.js
@@ -0,0 +1,16 @@
+const mode = document.getElementById('mode_icon');
+
+mode.addEventListener('click', () =>{
+    const form = document.getElementById('login_form');
+    if(mode.classList.contains('bi-moon-fill')) {
+        mode.classList.remove('bi-moon-fill');
+        mode.classList.add('bi-sun-fill');
+
+        form.classList.add('dark');
+        return
+    }
+
+    mode.classList.add('bi-moon-fill');
+    mode.classList.remove('bi-sun-fill');
+    form.classList.remove('dark');
+});
